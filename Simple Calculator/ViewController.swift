40953568d6989118b8577fcf8f4b46a6124d7d9d
handenuryavas <hande.nur038@gmail.com>
2023-01-12 14:14:33 +0300

    Initial Commit

---
+++ Simple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by Hande Nur Yavaş on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
